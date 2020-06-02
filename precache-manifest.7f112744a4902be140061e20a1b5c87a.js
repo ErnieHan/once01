7cb0dabc4ab72621126d3ba13fb64c696fee443f
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2020-06-02 16:12:40 +0800

    Updates

---
+++ precache-manifest.7f112744a4902be140061e20a1b5c87a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d2ab8451377debf8a2b8c4901a26de",
+    "url": "/once01/index.html"
+  },
+  {
+    "revision": "9540a7b6aff003371520",
+    "url": "/once01/static/css/main.6ecc3237.chunk.css"
+  },
+  {
+    "revision": "3e2bc3279ec53ad0f15a",
+    "url": "/once01/static/js/2.d6b105ab.chunk.js"
+  },
+  {
+    "revision": "14747f09b6c8b695bb30",
+    "url": "/once01/static/js/3.b8e9dc14.chunk.js"
+  },
+  {
+    "revision": "c33d8cfa397a6d968cfe",
+    "url": "/once01/static/js/4.5dc0cfbb.chunk.js"
+  },
+  {
+    "revision": "e4d9c75424fa2ef263fe",
+    "url": "/once01/static/js/5.6249a416.chunk.js"
+  },
+  {
+    "revision": "9540a7b6aff003371520",
+    "url": "/once01/static/js/main.966fcc37.chunk.js"
+  },
+  {
+    "revision": "3ebb400077feadfc7a37",
+    "url": "/once01/static/js/runtime-main.249baeb1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/once01/precache-manifest.158058ec4de617a18049316723c29d86.js"
+  "/once01/precache-manifest.7f112744a4902be140061e20a1b5c87a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/once01/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
